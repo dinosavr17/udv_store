65ca1f34437db0396846883d5b268e84b58c1bad
Dina Popova <studypost1701@gmail.com>
2022-04-09 23:51:48 +0500

    Adaptive navigation+Burger Sidebar

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Navbar from "./components/Navbar";
 import {Orders} from "./pages/Orders";
 import {UserInfo} from "./pages/UserInfo";
 import {SidebarData} from "./components/SidebarData";
+import {SignUp} from "./pages/SignUp";
 function App() {
   return (
       <BrowserRouter>
+          <Route path={'/sign-up'} component={SignUp}></Route>
           <Navbar/>
-          <div className="container">
        <Switch>
            <Route path={'/'} exact component={Home}></Route>
            <Route path={'/cart'} component={Cart}></Route>
@@ -18,7 +19,6 @@ function App() {
            <Route path={'/about'} component={UserInfo}></Route>
            <Route path={'/sidebar'} component={SidebarData}></Route>
        </Switch>
-          </div>
       </BrowserRouter>
   );
 }
---
+++ src/pages/SignUp.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from "react";
+import Register from "../components/Register";
+export const SignUp = () => {
+    return (
+        <Fragment>
+            <Register/>
+        </Fragment>
+    )
+
+}
